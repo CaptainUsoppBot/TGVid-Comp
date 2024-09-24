b53c93781ccc54961200074618f5d2eaa6d9e7a5
CaptainUsoppBot <inanstcompany@gmail.com>
2024-09-24 09:01:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "26254064")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7681393667:AAHRsSpyRAxLhUbRfGN9eDSWWWcRL-hv1yo") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7681393667:AAHjteEcAAxfo3aZwMKh75ahPnRk6HaS3jY") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002125561929') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
     DB_URL  = os.environ.get("DB_URL", "mongodb+srv://abidabdullahown7:abidabdullah1425@cluster0.7lgug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
+    DB_NAME  = os.environ.get("DB_NAME","Cluster0") 
 
     # Other Configs 
     ADMIN = int(os.environ.get("ADMIN", "5296584067")) # ⚠️ Required
